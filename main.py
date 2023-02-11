27577591971d95d5ae6b4266b1e064d35aeeb6c2
deivi1195 <deivig1195@gmail.com>
2023-02-11 13:15:22 -0400

    Creada la ruta PUT para actualizar un item

--- main.py
+++ main.py
@@ -7,17 +7,19 @@ app = FastAPI()
 
 class Item(BaseModel):
     name: str
-    price: Float
-    is_offer: Union[Bool, None] = None
+    price: float
+    is_offer: Union[bool, None] = None
 
 
 @app.get("/")
 def read_root():
     return {"Hola": "Mundo"}
 
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q : Union[str, None] = None):
     return {"itemd_id": item_id, "q": q}
 
+@app.put('/items/{item_id}')
+def update_item(item_id: int, item: Item):
+    return {'item_name': item.name, 'item_id': item_id, 'item_price': item.price}
 
