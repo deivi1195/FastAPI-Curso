2164efb44c9e9c0a4c9a0cd4da2d1b1bd02cd326
deivi1195 <deivig1195@gmail.com>
2023-02-11 22:15:15 -0400

    Funcion para tener el nombre completo de una persona

---
+++ S04_types/s04_01_autocompletado.py
@@ -0,0 +1,7 @@
+def get_full_name(first_name, last_name):
+    full_name = first_name.title() + ' ' + last_name.title()
+    return full_name
+
+
+print(get_full_name('Jhon', 'Ortiz'))  #Jhon Ortiz
+#print(get_full_name('Jhon', 0))  #Attributeerror (la funcion title() necesita un parametro de tipo str)
