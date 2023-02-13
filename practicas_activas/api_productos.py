1a764239c9712a6cb3b0f5ff72f46970f5b85888
deivi1195 <deivig1195@gmail.com>
2023-02-13 16:33:09 -0400

    Clase para representar un producto

--- practicas_activas/api_productos.py
+++ practicas_activas/api_productos.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI
-
 #NOTA IMPORTANTE DEL PROGRAMA
 #Los datos de los productos no se guardaran en un repositorio persistente.
 #Solo se mantendran temporalmente almacenaos en la memoria de trabajo (RAM)
 
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class productos(BaseModel):
+    id: Optional[str]
+    nombre: str
+    precio_compra: float
+    precio_venta: float
+    proveedor: str
+
 app = FastAPI()
+
+productos = []
 
 @app.get('/')
 def index():
